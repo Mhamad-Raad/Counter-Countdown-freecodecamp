eb9d69bf5f40fdd29b5cdfefb4de52cd3da2be2e
joker69216 <hamaraad883@gmail.com>
2022-12-18 15:15:03 +0300

    finalize the project

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ import "./App.css";
 function App() {
   const [breakLength, setBreakLength] = useState({
     second: 0,
-    minute: 5,
+    minute: 1,
   });
   const [sessionLength, setSessionLength] = useState({
     second: 0,
-    minute: 25,
+    minute: 1,
   });
 
   const [isPlaying, setIsPlaying] = useState(false);
--- src/Components/CountdownSession.jsx
+++ src/Components/CountdownSession.jsx
@@ -3,10 +3,12 @@ import css from "./CountdownSession.module.css";
 import React from "react";
 
 export default function CountdownSession({ currentSession, timeLeft }) {
+  const style = timeLeft.minute === 0 && timeLeft.second < 60 ? css.danger : null;
+
   return (
     <div className={css.countdownBg}>
-      <h3 className={css.currentSession}>{currentSession}</h3>
-      <h3 className={css.timeLeft}>
+      <h3 className={`${css.currentSession} ${style}`}>{currentSession}</h3>
+      <h3 className={`${css.timeLeft} ${style}`}>
         {`
       ${timeLeft.minute < 10 ? `0${timeLeft.minute}` : `${timeLeft.minute}`}: 
       ${timeLeft.second < 10 ? `0${timeLeft.second}` : `${timeLeft.second}`}`}
