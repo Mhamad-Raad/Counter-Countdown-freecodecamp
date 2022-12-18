b69bcd9e01e93ff72d8bb0842ae0c18eec6644e0
joker69216 <hamaraad883@gmail.com>
2022-12-18 12:00:57 +0300

    add timelines

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import { useState } from "react";
 import Title from "./Components/Title";
+import TimeLength from "./Components/TimeLength";
 import "./App.css";
 
 function App() {
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+
+  const incrementBreak = () => {
+    setBreakLength((prev) => prev + 1);
+  };
+
+  const decrementBreak = () => {
+    setBreakLength((prev) => prev - 1);
+  };
+
+  const incrementSession = () => {
+    setSessionLength((prev) => prev + 1);
+  };
+
+  const decrementSession = () => {
+    setSessionLength((prev) => prev - 1);
+  };
+
   return (
     <div className="App">
       <Title />
       <div className="lengths">
-        
+        <TimeLength
+          title="Break"
+          time={breakLength}
+          increment={incrementBreak}
+          decrement={decrementBreak}
+        />
+        <TimeLength
+          title="Session"
+          time={sessionLength}
+          increment={incrementSession}
+          decrement={decrementSession}
+        />
       </div>
     </div>
   );
--- src/Components/TimeLength.jsx
+++ src/Components/TimeLength.jsx
@@ -1,9 +1,21 @@
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 import css from "./TimeLength.module.css";
 
-export default function TimeLength({title, time, increment, decrement}) {
+export default function TimeLength({ title, time, increment, decrement }) {
   return (
     <div className={css.length}>
-      <h2 className={css.Ltitle}>{title}</h2>
+      <h2 className={css.Ltitle}>{title} Length</h2>
+      <div className={css.LtimeBg}>
+        <FaArrowDown
+          className={(css.LtimeGroup, css.arrowBtn)}
+          onClick={decrement}
+        />
+        <p className={css.LtimeGroup}>{time}</p>
+        <FaArrowUp
+          className={(css.LtimeGroup, css.arrowBtn)}
+          onClick={increment}
+        />
+      </div>
     </div>
-  )
+  );
 }
